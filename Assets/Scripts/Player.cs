3fde4f1796a0d7ff9efef3fac1d83d963899885e
foxter888 <paul.southworth888@yahoo.com>
2023-01-18 15:11:18 -0800

    Jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,29 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float speed = 1;
+    [SerializeField] private float jumpForce = 200;
 
 
     void Update()
     {
         var horizontal = Input.GetAxis("Horizontal") * speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
-
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
         Debug.Log($"Velocity = {rigidbody2D.velocity}");
 
         var animator = GetComponent<Animator>();
         bool walking = horizontal != 0;
         animator.SetBool("Walk", walking);
+
+        if (horizontal != 0)
+        {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = horizontal < 0;
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce);
+        }
     }
 }
