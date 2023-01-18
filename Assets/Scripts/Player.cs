81c5757d43b5ea50063200d88341b636708899fc
foxter888 <paul.southworth888@yahoo.com>
2023-01-18 14:25:05 -0800

    added variable speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] private float speed = 1;
+
+
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxis("Horizontal") * speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
 
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+        Debug.Log($"Velocity = {rigidbody2D.velocity}");
     }
 }
