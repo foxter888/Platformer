ab99d3d872406763a2bb85f05c79edcfe25c678f
foxter888 <paul.southworth888@yahoo.com>
2023-01-18 15:18:14 -0800

    adjusted the jumping and added a v ariable for it

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] private float speed = 1;
-    [SerializeField] private float jumpForce = 200;
+    [SerializeField] private float _speed = 1;
+    [SerializeField] private float _jumpForce = 200;
 
 
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal") * speed;
+        var horizontal = Input.GetAxis("Horizontal") * _speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
         Debug.Log($"Velocity = {rigidbody2D.velocity}");
@@ -28,7 +28,7 @@ public class Player : MonoBehaviour
 
         if(Input.GetButtonDown("Fire1"))
         {
-            rigidbody2D.AddForce(Vector2.up * jumpForce);
+            rigidbody2D.AddForce(Vector2.up * _jumpForce);
         }
     }
 }
