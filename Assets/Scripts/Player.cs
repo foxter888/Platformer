6a9b47c23668e6ebf0b9c192a4e4a32e7f4cfb78
foxter888 <paul.southworth888@yahoo.com>
2023-01-18 14:47:15 -0800

    Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,5 +15,9 @@ public class Player : MonoBehaviour
 
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
         Debug.Log($"Velocity = {rigidbody2D.velocity}");
+
+        var animator = GetComponent<Animator>();
+        bool walking = horizontal != 0;
+        animator.SetBool("Walk", walking);
     }
 }
