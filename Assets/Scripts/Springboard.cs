92b1f88f9bf802f994155aac64514b6d799afd46
MSI\pauls <paul.southworth888yahoo.com>
2023-02-26 05:30:23 -0800

    Added sprinboard and removed player friction

---
+++ Assets/Scripts/Springboard.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Springboard : MonoBehaviour
+{
+    [SerializeField] float _bounceVelocity = 10;
+
+    SpriteRenderer _spriteRenderer;
+    [SerializeField] Sprite _downSprite;
+    Sprite _upSprite;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _upSprite = _spriteRenderer.sprite;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var player = collision.collider.GetComponent<Player>();
+        if (player != null)
+        {
+            var rigidbody2D = player.GetComponent<Rigidbody2D>();
+
+            if (rigidbody2D != null)
+            {
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _bounceVelocity);
+                _spriteRenderer.sprite = _downSprite;
+            }
+
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        var player = collision.collider.GetComponent<Player>();
+        if (player != null)
+        {
+            _spriteRenderer.sprite = _upSprite;
+        }
+    }
+}
